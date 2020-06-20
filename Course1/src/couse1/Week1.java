40e6a7e69f978d13021bf4f0408bf1bcc9d2fe2c
saigontrade88 <longdangvn88@gmail.com>
2020-06-19 23:23:57 -0400

    Successfully complete week 1.

--- Course1/src/couse1/Week1.java
+++ Course1/src/couse1/Week1.java
@@ -1,6 +1,7 @@
 package couse1;
 import java.io.*;
 import java.lang.Math.*;
+import java.math.*;
 
 public class Week1 {
 	
@@ -11,62 +12,78 @@ public class Week1 {
 		return result;
 	}
 	
-	public static int Karatsuba_Multiplication(int x, int y, int n) {
+	public static BigInteger Karatsuba_Multiplication(BigInteger x, BigInteger y, int n) {
 		
-		if(n == 2)
-			return x*y;
+		if(n <= 2)
+			return x.multiply(y);
 		
-		int a = x / pow(10, n/2);
-		int b = x % pow(10, n/2) ;
-		int c = y / pow(10, n/2);
-		int d = y % pow(10, n/2);
+		BigInteger mySplit = BigInteger.valueOf(10).pow(n/2);
 		
-		int ac = Karatsuba_Multiplication(a, c, n/2);
-		int bd = Karatsuba_Multiplication(b, d, n/2);
-		int term3 = Karatsuba_Multiplication(a+b, c+d, n/2);
+		BigInteger a = x.divide(mySplit);
 		
-		int term4 = term3 - ac - bd;
+		BigInteger b = x.subtract(mySplit.multiply(a));
 		
-		System.out.printf("\na= %d", a); 
-		System.out.printf("\nb= %d", b); 
-		System.out.printf("\nc= %d", c); 
-		System.out.printf("\nd= %d", d);
+		BigInteger c = y.divide(mySplit);
 		
-		System.out.printf("\nac= %d", ac);
-		System.out.printf("\nbd= %d", bd);
-		System.out.printf("\n\\(a+b)(c+d)= %d", term3);
-		System.out.printf("\nterm4= %d", term4);
+		BigInteger d = y.subtract(mySplit.multiply(c));
+		
+		BigInteger ac = Karatsuba_Multiplication(a, c, n/2);
+		BigInteger bd = Karatsuba_Multiplication(b, d, n/2);
+		BigInteger term3 = Karatsuba_Multiplication(a.add(b), c.add(d), n/2);
+		
+		BigInteger term4 = term3.subtract(ac).subtract(bd);
+		
+		
+		  System.out.printf("\na= %d", a); System.out.printf("\nb= %d", b);
+		  System.out.printf("\nc= %d", c); System.out.printf("\nd= %d", d);
+		  
+		  System.out.printf("\nac= %d", ac); System.out.printf("\nbd= %d", bd);
+		  System.out.printf("\n\\(a+b)(c+d)= %d", term3);
+		  System.out.printf("\nterm4= %d", term4);
+		 
 			
-		return pow(10, n) * ac + pow(10,n/2)*(term4) + bd;
+		BigInteger rTerm1 = ac.multiply(BigInteger.valueOf(10).pow(n));
+		BigInteger rTerm2 = term4.multiply(mySplit);
+		
+		return rTerm1.add(rTerm2).add(bd);
 		
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		int n = 0, temp = 0;
-		BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Please enter the size of your array: ");
-		n = Integer.parseInt(consoleInput.readLine());
-		int[] myArray = new int[n];
+		//BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int i = 0; i < n; i++) {
-			System.out.printf("Please enter the %d element of your array: \n", (i + 1));
-			temp = Integer.parseInt(consoleInput.readLine());
-			myArray[i] =  temp;
-			temp = 9999;
-		}
+		BufferedReader myInFile = new BufferedReader(new FileReader(args[0]));
 		
+		String currentLine = myInFile.readLine();
 		
-		System.out.printf("Your array has %d elements\n", myArray.length);
-		System.out.println("Your array has the following elements:"); 
+	    n = currentLine.length();
+	    
+	    //System.out.printf("n = %d\n", n);
 
-		for (int j = 0; j < myArray.length; j++) {
-			System.out.printf("\nj = %d, myArray[%d] = %d", j + 1, j + 1, myArray[j]); 
-		}
+	    //System.out.println(currentLine);
 		
-		System.out.printf("\nThe product of your numbers = %d", Karatsuba_Multiplication(myArray[0], myArray[1], 4));
+	    BigInteger a = new BigInteger(currentLine);
 		
+		currentLine = myInFile.readLine();
 		
+		//System.out.println(currentLine);
+		
+		BigInteger b = new BigInteger(currentLine);
+		
+		myInFile.close();
+		
+		System.out.printf("a = %d\n", a);
+	
+		System.out.printf("b = %d\n", b);
+		
+		System.out.printf("n = %d\n", n);
+		
+		BigInteger result = Karatsuba_Multiplication(a, b, n);
+		
+		System.out.printf("\nresult = %d", result);
+			
 	}
 
 }
