d311f425783edf1dc146b4a1d93c15404e8b2621
saigontrade88 <longdangvn88@gmail.com>
2020-06-23 23:29:26 -0400

    Complete Week 2

---
+++ Course1/runTestCases_2.sh
@@ -0,0 +1,2 @@
+cd /media/long/DATA/Khongxoa/Java_Workspaces/stanford-algs
+java -cp /media/long/DATA/Khongxoa/Java_Workspaces/Coursera-Stanford-Divide-and-Conquer-Sorting-and-Searching-and-Randomized-Algorithms/Course1/bin:/media/long/DATA/Khongxoa/Java_Workspaces/stanford-algs/ tester/java/Tester couse1.Week2 testCases/course1/assignment2Inversions/input*
--- Course1/src/couse1/Week2.java
+++ Course1/src/couse1/Week2.java
@@ -1,12 +1,13 @@
 package couse1;
 import java.io.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Week2 {
 	
-	public static int CountSplitInv(ArrayList<Integer> myArray, int startIndex, int midIndex, int endIndex) {
-		System.out.printf("\tCountSplitInv, start = %d, mid = %d, end = %d\t", startIndex, midIndex, endIndex);
-		int invCount = 0;
+	public static BigInteger CountSplitInv(ArrayList<Integer> myArray, int startIndex, int midIndex, int endIndex) {
+		//System.out.printf("\tCountSplitInv, start = %d, mid = %d, end = %d\t", startIndex, midIndex, endIndex);
+		BigInteger invCount = BigInteger.ZERO;
 		int n1 = midIndex - startIndex + 1;
 		int n2 = endIndex -  midIndex;
 		ArrayList<Integer> left, right;
@@ -33,24 +34,27 @@ public class Week2 {
 			else {
 				myArray.set(k,right.get(j));
 				j++;
-				invCount += left.size() - i - 1;
-				System.out.printf("k = %d, invCount = %d\n", k, invCount);
+				//invCount += left.size() - i - 1;
+				BigInteger remains = BigInteger.valueOf(left.size() - i - 1);
+				invCount = invCount.add(remains);
+				//System.out.printf("k = %d, invCount = %d\n", k, invCount);
 			}
 		}
 		return invCount;
 	}
 	
-	public static int SortAndCount(ArrayList<Integer> myArray, int startIndex, int endIndex) {
-		System.out.printf("\tSortAndCount, start = %d, end = %d", startIndex, endIndex);
-		int totalCount = 0, firstCount = 0, secondCount = 0, thirdCount = 0;
+	public static BigInteger SortAndCount(ArrayList<Integer> myArray, int startIndex, int endIndex) {
+		//System.out.printf("\tSortAndCount, start = %d, end = %d", startIndex, endIndex);
+		BigInteger totalCount = BigInteger.ZERO, firstCount = BigInteger.ZERO, secondCount = BigInteger.ZERO, thirdCount = BigInteger.ZERO;
+		
 		if (startIndex < endIndex) {
 			int mid = (startIndex + endIndex)/2;
 			firstCount = SortAndCount(myArray, startIndex, mid);
 			secondCount = SortAndCount(myArray, mid + 1, endIndex);
 			thirdCount = CountSplitInv(myArray, startIndex, mid, endIndex);
 		}
-		totalCount = firstCount + secondCount + thirdCount;
-		System.out.printf("\tSortAndCount, firstCount = %d, secondC = %d, thirdC = %d\n", firstCount, secondCount, thirdCount);
+		totalCount = firstCount.add(secondCount).add(thirdCount);
+		//System.out.printf("\tSortAndCount, firstCount = %d, secondC = %d, thirdC = %d\n", firstCount, secondCount, thirdCount);
 		return totalCount;
 	}
 
@@ -58,7 +62,7 @@ public class Week2 {
 		// TODO Auto-generated method stub
 		// Open the file that is the first command line parameter
 	try {
-		args[0] = "Input/input_beaunus_1_4.txt";
+		//args[0] = "Input/input_beaunus_1_4.txt";
 		
 		ArrayList<Integer> myArray = new ArrayList<Integer>();
 		
@@ -81,8 +85,8 @@ public class Week2 {
 //			System.out.println(myArray.get(k));
 //		}
 		
-		System.out.printf("inverse count = %d", SortAndCount(myArray, 0, myArray.size() - 1));
-		
+		//System.out.printf("inverse count = %d", SortAndCount(myArray, 0, myArray.size() - 1));
+		System.out.print(SortAndCount(myArray, 0, myArray.size() - 1));
 	}catch (Exception e){//Catch exception if any
 		System.err.println("Error: " + e.getMessage());
 	}
