cbc9c9bb61619b04a45e7d0290c8d564eb994585
saigontrade88 <longdangvn88@gmail.com>
2020-07-06 14:22:31 -0400

    Modify constructor for GraphEdge class

--- Course1/src/couse1/couse1/week4/GraphEdge.java
+++ Course1/src/couse1/couse1/week4/GraphEdge.java
@@ -1,5 +1,13 @@
 package couse1.week4;
 
 public class GraphEdge {
+	
+	GraphVertex v0,v1;
 
+	public GraphEdge( GraphVertex _v0, GraphVertex _v1, float _weight ){
+		v0 = _v0;
+		v1 = _v1;
+
+	}
+	  
 }
