7f69d9979e2f3a27226e579d8a0e06116b31b06f
saigontrade88 <longdangvn88@gmail.com>
2020-07-20 14:54:40 -0400

    Clean up code for future reference in Week 4's program

--- Course1/src/couse1/couse1/week4/Week4.java
+++ Course1/src/couse1/couse1/week4/Week4.java
@@ -39,11 +39,15 @@ public class Week4 {
 			
 			//args[0] = "Course1/Input/Wk4_input_random_0_4.txt";
 			
-			args[0] = "Course1/Input/Wk4_input_random_1_6.txt";
-			
-			//args[0] = "Course1/Input/Wk4_kargerMinCut.txt";
-			
-			//args[0] = "Course1/Input/Wk4_input_random_1_8_Forum.txt";
+//			args[0] = "Course1/Input/Wk4_input_random_1_6.txt";
+//					
+//			args[0] = "Course1/Input/Wk4_input_random_1_8_Forum.txt";
+//			
+//			args[0] = "Course1/Input/Wk4_input_random_5_40_Forum.txt";
+//			
+//			args[0] = "Course1/Input/Wk4_input_random_6_8_Forum.txt";
+//			
+			args[0] = "Course1/Input/Wk4_kargerMinCut.txt";
 			
 			//Karger's parameters = 10000000;
 			long numberMinCut = 10000000;
@@ -156,11 +160,11 @@ public class Week4 {
 		    
 		   // myGraph.printGraph();
 		    
-		    //for (int i = 0; i < Math.pow(verts.size(), 2); i++) {
-		    for (int i = 0; i < 2  ; i++) {
+		    //for (int i = 0; i < (int) Math.ceil(Math.pow(verts.size(), 2) * Math.log(verts.size())); i++) {
+		    for (int i = 0; i < 400  ; i++) {
 		    	long temp = myGraph.kargerMinCut();
 		    	//PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
-		    	System.out.println(i + "= " + temp);
+		    	//System.out.println(i + "= " + temp);
 		    	//System.setOut(out);
 		    	
 		    	if(temp < numberMinCut)
